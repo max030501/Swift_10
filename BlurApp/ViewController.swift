244cb08b406469416da1eb7d87660afdc792069e
max030511 <max030511@gmail.com>
2021-06-16 06:08:55 -0700

    done

--- BlurApp/ViewController.swift
+++ BlurApp/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let imageSet = ["cloud", "coffe", "food"]
+    var blurEffectView: UIVisualEffectView?
+    @IBOutlet weak var backgroundImageView: UIImageView!
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()	
+        let selectedImageIndex = Int(arc4random_uniform(3))
+        // Apply blurring effect
+        backgroundImageView.image = UIImage(named: imageSet[selectedImageIndex])
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView?.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView!)
         // Do any additional setup after loading the view.
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        blurEffectView?.frame = view.bounds
     }
 
 
